65aa352fa64c53d1d2aec22c02a17e41fac5d29d
Shantanu Dubey <shantanu.dubey1@outlook.com>
2023-05-13 10:43:32 +0530

    Update floordiv_modulus.py
    added more info  in comments

--- lup/floordiv_modulus.py
+++ lup/floordiv_modulus.py
@@ -1,5 +1,19 @@
-#special operations
-#Ref : https://www.codingem.com/modulo-in-python/
+"""
+Excerpt from #ref : https://www.codingem.com/modulo-in-python/
+To recap, a modulo b in mathematics calculates the remainder in the division between a and b.
+
+For example, 7 mod 3 represents sharing 7 apples with 3 workers evenly. The result of 7 mod 3 is 1, that is, one apple is going to be leftover.
+
+In Python, a common way to calculate modulo is using the dedicated modulo operator %.
+Alternatively, if you want to know both the result of the division and the remainder, you can use the built-in divmod() function.
+When doing modular arithmetic with floats, use the math module’s fmod() function.
+Modulos also work for negative numbers in Python. However, the way negative modulos are calculated can differ from language to language.
+
+There are many use cases for modulo in Python. For example, to figure out if a number is odd or even, you need to use modulo. Another common use case for modulo is to check if a number is a prime number.
+"""
+
+
+
 """
 Division : 
 	N / D = normal division happens and the resulting sign is based on "if both signs sre same then positive else negative"
